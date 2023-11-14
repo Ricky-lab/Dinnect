7ae27148ae34c4c4e068728d04514c5029a971d6
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2023-11-14 18:12:53 -0500

    Update: events pages and api

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
